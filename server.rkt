7091d1970414ff301585a094630af81ae4a6e09f
Stewart Park <hello@stewartjpark.com>
2016-10-07 17:07:32 -0700

    Print date

--- server.rkt
+++ server.rkt
@@ -15,9 +15,11 @@
   (string-join (list
     (number->string (date-year date))
     (~a (number->string (date-month date)) #:width 2 #:pad-string "0" #:align 'right)
-    (~a (number->string (date-day date)) #:width 2 #:pad-string "0" #:align 'right)
+    (~a (number->string (- (date-day date) 1)) #:width 2 #:pad-string "0" #:align 'right)
   ) "")
 )
+
+(display (format-date (current-date)))
 
 (define (get-lunch-description)
     (let-values [(
